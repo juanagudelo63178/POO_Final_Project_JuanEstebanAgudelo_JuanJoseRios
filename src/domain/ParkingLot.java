5cf417784290d226fe99b70e3d406c3319f384ce
juanjo-rs <juan.rios62744@ucaldas.edu.co>
2026-06-02 23:57:34 -0500

    Added ParkingLot, Ticket and Vehicle classes

--- src/domain/ParkingLot.java
+++ src/domain/ParkingLot.java
@@ -1,5 +1,23 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class ParkingLot {
-    
+    private ArrayList<Vehicle> vehicles;
+    private ArrayList<Ticket> tickets;
+    private ArrayList<ParkingSpot> parkingSpots;
+    private ArrayList<Employee> employees;
+
+    public ParkingLot(){
+        vehicles= new ArrayList<>();
+        tickets= new ArrayList<>();
+        parkingSpots= new ArrayList<>();
+        employees= new ArrayList<>();
+    }
+    public void registerEntry(Ticket ticket){
+        tickets.add(ticket);
+    }
+    public int getTotalTickets(){
+        return tickets.size();
+    }
 }
--- src/domain/Ticket.java
+++ src/domain/Ticket.java
@@ -1,5 +1,18 @@
 package domain;
 
-public class Ticket {
+import java.time.LocalDateTime;
 
+public class Ticket {
+    private String id;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+    private double fee;
+    
+    public Ticket(String id, LocalDateTime entryTime){
+        this.id= id;
+        this.entryTime= entryTime;
+    }
+    public double  getFee(){
+        return fee;
+    }
 }
