6c6a0b275fdcb1cc687fa8c4476742bce6639c9b
juanagudelo63178 <juan.agudelo63178@ucaldas.edu.co>
2026-06-04 15:35:32 -0500

    Added console menu and user interaction
    - Connected Main and Console classes
    - Added ParkingLot initialization
    - Implemented console menu
    - Added Scanner for user input
    - Added switch-case menu handling
    - Created menu option methods

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -1,5 +1,83 @@
 package ui;
 
+import domain.ParkingLot;
+
+import java.util.Scanner;
+
 public class Console {
-    
-}
+
+    private ParkingLot parkingLot;
+
+    private Scanner scanner;
+
+    public Console() {
+        parkingLot = new ParkingLot();
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+
+        System.out.println("=================================");
+        System.out.println("      PARKING LOT SYSTEM");
+        System.out.println("=================================");
+        System.out.println("1. Register Vehicle Entry");
+        System.out.println("2. Register Vehicle Exit");
+        System.out.println("3. Show Available Spots");
+        System.out.println("4. Generate Report");
+        System.out.println("0. Exit");
+        System.out.print("Select an option: ");
+        int option = scanner.nextInt();
+
+        System.out.println("You selected: " + option);
+
+        switch(option) {
+
+        case 1:
+            registerVehicleEntry();
+            break;
+
+        case 2:
+            registerVehicleExit();
+            break;
+
+        case 3:
+            showAvailableSpots();
+            break;
+
+        case 4:
+            generateReport();
+            break;
+
+        case 0:
+            System.out.println("Closing system...");
+            break;
+
+        default:
+            System.out.println("Invalid option");
+        }
+    }
+
+    private void registerVehicleEntry() {
+
+    System.out.println("Vehicle entry selected");
+
+    }
+
+    private void registerVehicleExit() {
+
+    System.out.println("Vehicle exit selected");
+
+    }
+
+    private void showAvailableSpots() {
+
+    System.out.println("Available spots selected");
+
+    }
+
+    private void generateReport() {
+
+    System.out.println("Report selected");
+
+    }
+}   
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,10 @@
 package ui;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Console console = new Console();
+        console.start();
+
+    }
 }
